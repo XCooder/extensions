3dcfaf3450c351c167cf8ba6f9a13d147fe68941
XCooder <2207952662@qq.com>
2019-01-24 01:05:03 +0800

    脉脉好友列表插件第一次提交

---
+++ friendList/js/background.js
@@ -0,0 +1,3 @@
+chrome.browserAction.onClicked.addListener(function () {
+    console.log('有动作了')
+});
---
+++ friendList/js/test.js
@@ -0,0 +1,14 @@
+
+$(function() {
+    function getFriendList() {
+        var friendArr = [];
+        var characterInfo = {
+    
+        };
+    
+        var list = $('.contact_infinit_scroller .selectable');
+    
+    }
+    
+    getFriendList();
+})
